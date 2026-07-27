f943bf4d2467722fd89f2bf742f17ddf7280b325
Prashanth Molakala <prashanth.molakala@gmail.com>
2026-07-26 19:32:34 -0500

    add api base env var

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,5 @@ declare global {
 
 export const config = {
   googleClientId: window.__ENV__?.GOOGLE_CLIENT_ID || import.meta.env.VITE_GOOGLE_CLIENT_ID || '',
+  apiUrl: window.__ENV__?.API_URL || '/api',
 };
